b1a7bcc71e8c4ad2dbfc98f24a6679a6ce545fe5
Jamshid Ismoilov <87127098+Jamshid-Ismoilov@users.noreply.github.com>
2021-10-19 16:53:36 +0500

    Add files via upload

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	h "app/handlers"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.POST("/signup", h.Signup)
+	r.POST("/login", h.Login)
+	r.POST("/changepassword", h.ChangePassword)
+
+	port := ":4000"
+
+	log.Println("serving: " + port)
+	r.Run(port)	
+}
